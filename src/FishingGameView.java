e7940cb8261cee3d0cb0d9edb1690ecdaacd16bb
rmccann01 <31964312+rmccann01@users.noreply.github.com>
2018-05-06 23:21:27 -0400

    Combine Research and Fishing Game

--- src/FishingGameView.java
+++ src/FishingGameView.java
@@ -27,7 +27,6 @@ import javax.swing.JPanel;
  * @author Joel
  */
 public class FishingGameView{
-	JFrame frame;
 	static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	final static int WIDTH = screenSize.width;// 1100;//786;//
 	final static int HEIGHT = screenSize.height; // 500;//
@@ -63,13 +62,8 @@ public class FishingGameView{
 		fishes.add(new Fish(Fish.Species.STURGEON, 1, 1, Direction.EAST));
 		hook = new Hook();
 		loadSprites();
-		frame = new JFrame();
-		frame.getContentPane().add(panel);
 		panel.setFocusable(true);
 		panel.requestFocusInWindow();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(WIDTH, HEIGHT);
-		frame.setVisible(true);
 		reeling = false;
 		gameOver = false;
 	}
