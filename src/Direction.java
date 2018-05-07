107fa8463274ddb00def22afde1eab5d12be81d0
Joel Turk <jturk@udel.edu>
2018-05-06 20:09:38 -0400

    Forgot to stage files

--- src/Direction.java
+++ src/Direction.java
@@ -2,22 +2,23 @@
  * This enum is a description of which direction an object is facing
  * and therefore, where it should move.
  */
-
 public enum Direction {
-	NORTH("_north"),
-	NORTHEAST("_northeast"),
 	EAST("_east"),
-	SOUTHEAST("_southeast"),
-	SOUTH("_south"),
-	SOUTHWEST("_southwest"),
-	WEST("_west"),
-	NORTHWEST("_northwest");
+	WEST("_west");
 	
 	private String name = null;
-	
+	/**
+	 * Creates a new Direction with a name
+	 * @param s a String which is a partial directory of the Direction (for loading images)
+	 */
 	private Direction(String s){
 		name = s;
 	}
+	
+	/**
+	 * returns the name of a Direction
+	 * @return a String, the name
+	 */
 	public String getName() {
 		return name;
 	}
